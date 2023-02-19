123467384233f504ddf85703c3a6de83b65292b4
Kacper Głodowski <kacperglodowski@wp.pl>
2023-02-20 00:49:40 +0100

    symfony prepare file

--- symfony/prepare.sh
+++ symfony/prepare.sh
@@ -1,1 +1,3 @@
-composer install && npm install -y && php bin/console -n do:mi:mi && npm run build
+composer install && npm install -y && php bin/console -n do:mi:mi && \
+php bin/console do:qu:sql "INSERT INTO \"user\" (roles,password,login) VALUES('[\"ROLE_ADMIN\",\"ROLE_USER\"]','\$2y\$13\$cpQyh5.XtMonsiRFjzWc5ucQz.Hvwppl85Lnwp8LTBgxWzbkYruvK','admin');" && \
+npm run build
